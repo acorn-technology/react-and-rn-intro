33f703247f2002eeebeecec9592d52866267af73
kenalexandersson <ken.alexandersson@gmail.com>
2018-09-11 20:40:27 +0200

    Fixed indentation

--- tutorial/react/source/solution/my-app/src/components/search-bar.js
+++ tutorial/react/source/solution/my-app/src/components/search-bar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class SearchBar extends Component {
 
@@ -8,15 +8,15 @@ class SearchBar extends Component {
         this.state = {searchTerm: ''};
     }
 
-	render() {
-		return (
-		    <div className="search-bar">
-		        <input 
-					value={this.state.searchTerm}
-					onChange={event => this.onInputChange(event.target.value)} />
+    render() {
+        return (
+            <div className="search-bar">
+                <input
+                    value={this.state.searchTerm}
+                    onChange={event => this.onInputChange(event.target.value)}/>
             </div>
         );
-	}
+    }
 
     onInputChange(searchTerm) {
         this.setState({searchTerm: searchTerm});
