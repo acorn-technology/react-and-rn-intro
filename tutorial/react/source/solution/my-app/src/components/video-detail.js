000153724e45b41e97dcf953e3086658479d2a4b
Ken Alexandersson <ken.alexandersson@jeppesen.com>
2018-09-13 15:35:03 +0200

    Added fullscreen mode + nifty coloring

--- tutorial/react/source/solution/my-app/src/components/video-detail.js
+++ tutorial/react/source/solution/my-app/src/components/video-detail.js
@@ -14,7 +14,7 @@ const VideoDetail = ({video}) => {
     return (
         <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
-                <iframe className="embed-responsive-item" title="video-detail" src={videoUrl}/>
+                <iframe className="embed-responsive-item" title="video-detail" src={videoUrl} allowFullScreen="allowFullScreen" />
             </div>
             <div className="details">
                 <div className="video-title">{video.snippet.title}</div>
