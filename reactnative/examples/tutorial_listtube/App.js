f190573dbe2c540c5c30b52b22a9d88796589d1d
Chris Fogelklou <chris.fogelklou@gmail.com>
2018-09-18 06:26:00 +0200

    wip

--- reactnative/examples/tutorial_listtube/App.js
+++ reactnative/examples/tutorial_listtube/App.js
@@ -1,49 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
+/** @flow */
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Text, View, Button, ListView, Image, TouchableOpacity, Alert} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
+// Flow type declarations
+type Contact = {name: string, number:string, pic:string};
 type Props = {};
-export default class App extends Component<Props> {
+type State = { contacts: Array<Contact>, ds:any};
+
+// Class declaration including the component types.
+export default class App extends Component<Props, State> {
+  ds:any;
+  state:State = { contacts:[], ds:[] };
+
+  constructor() {
+    super()
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = { contacts:[], ds:[] };
+    this.state = this.addOneContact(true);
+  }
+  addOneContact(isConstructor:?boolean):State{
+    let contact:Contact = {name:getName(), number:getNumber(), pic:getPic()};
+    let contacts = [...this.state.contacts, contact];
+    let ds = this.ds.cloneWithRows(contacts);
+    let newState:newState = {contacts:contacts, ds:ds};
+    if (!isConstructor){
+      this.setState(newState);
+    }
+    return newState;
+  }
+  renderRow(rowData:Contact, unused:string, index:string){
+    return (
+      <TouchableOpacity style={{backgroundColor:'#E0FFFF', margin:1,
+        flexDirection:'row', alignItems:'center'}}
+        onPress={()=>{
+          Alert.alert(
+            'Calling ' + rowData.name,
+            'Are you sure?',
+            [
+              {text: 'Cancel', onPress: () => console.log('Call Canceled')},
+              {text: 'OK', onPress: () => console.log('Called ' + rowData.name)},
+            ],
+          )
+        }}
+        >
+        <Image style={{margin:1, width: 51, height: 51}}
+          source={{uri: rowData.pic}} />
+        <Text style={{flex:1, marginLeft:10}}>{rowData.name}</Text>
+        <Text style={{flex:1}}>{rowData.number}</Text>
+      </TouchableOpacity>
+    );
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{flex:1, alignItems: 'stretch'}}>
+        <ListView style={{flex:1, marginTop:20}} dataSource={this.state.ds}
+          renderRow={(rowData, unused, index) => {
+            return this.renderRow(rowData, unused, index);}} />
+        <Button title="Add One"
+          onPress={()=>{this.addOneContact()}} style={{alignSelf:'center'}} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ reactnative/examples/tutorial_listtube/listtube_presentation.md
@@ -0,0 +1,48 @@
+---
+presentation:
+  width: 1440
+  height: 1280
+  controls: false
+---
+
+<!-- slide -->
+- Add some new dependencies to support youtube and start rebuilding (could take a few minutes, so build in the background)
+
+1. Using NPM:
+```bash
+npm install --save react-native-elements
+npm install --save react-native-vector-icons
+npm install --save react-native-youtube
+npm install --save youtube-api-search
+react-native link
+```
+
+2. Or Using Yarn:
+```bash
+yarn add --save react-native-elements
+yarn add --save react-native-vector-icons
+yarn add --save react-native-youtube
+yarn add --save youtube-api-search
+react-native link
+```
+
+3. Or Manually Updating package.json
+
+```json
+  "dependencies": {
+    "react": "16.5.0",
+    "react-native": "0.57.0",
+    "react-native-elements": "^0.19.1",
+    "react-native-vector-icons": "^4.2.0",
+    "react-native-youtube": "^1.1.0",
+    "youtube-api-search": "^0.0.5"
+  },
+```
+3.1 Followed by letting npm update modules:
+```bash
+npm install
+react-native link
+```
+or `yarn`
+
+Finally: `react-native run-ios` or `react-native run-android`
